dd8242806313b2e934a8f2ccd52ab5e6169c8e3b
Mathivanan <mathivanansakthivel10@gmail.com>
2023-06-05 06:27:11 +0530

    working with sample testNG Project

---
+++ src/testNG/SampleTestNG.java
@@ -0,0 +1,26 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class SampleTestNG {
+	@Test
+	public void secondCase() {
+		System.out.println("secondCase");
+	}
+
+	@Test
+	public void thirdCase() {
+		System.out.println("ThirdCase");
+	}
+
+	@Test
+	public void fourthCase() {
+		System.out.println("FourthCase");
+	}
+
+	@Test
+	public void fivethCase() {
+		System.out.println("FivethCase");
+	}
+
+}
